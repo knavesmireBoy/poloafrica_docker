475bf211c536defdd16456b4b0a24d6e20bfc46d
Andrew Sykes <andrewsykes@btinternet.com>
2024-07-10 13:47:52 +0100

    stil messin

--- websites/default/templates/__section.html.php
+++ websites/default/templates/__section.html.php
@@ -10,14 +10,10 @@ if (isset($myasset)) {
   //  $incfile = validate_extension(trim($myasset->path), VIDEO_EXT) ? 'video' : 'image';
 }
 
-
+include '_accordion.html.php';
 foreach ($article->assets as $k => $myasset) {
     $incfile = validate_extension(trim($myasset->path), VIDEO_EXT) ? 'video' : 'image';
-    $finish = !isset($article->assets[$k+1]);
-    if ($incfile === 'video') {
-        include '_videos.html.php';
-    } else {
-        include doInclude($article->content);
-    }
-    $ran = true;
+    $flush = !isset($article->assets[$k+1]);
 }
+
+include '_foobar.html.php';
